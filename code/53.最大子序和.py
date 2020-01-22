296f8f5b08016311b4599545bf9eeae3cf93ea72
xjcai <xjcai18@fudan.edu.cn>
2020-01-22 20:07:51 +0800

    动态规划加贪心算法

---
+++ code/53.最大子序和.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode.cn id=53 lang=python3
+#
+# [53] 最大子序和
+#
+# https://leetcode-cn.com/problems/maximum-subarray/description/
+#
+# algorithms
+# Easy (48.52%)
+# Likes:    1460
+# Dislikes: 0
+# Total Accepted:    139K
+# Total Submissions: 286.5K
+# Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+#
+# 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+# 
+# 示例:
+# 
+# 输入: [-2,1,-3,4,-1,2,1,-5,4],
+# 输出: 6
+# 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+# 
+# 
+# 进阶:
+# 
+# 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+# 
+#
+
+# @lc code=start
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        #递归终止条件
+        if n == 1:
+            return nums[0]
+        else:
+            #递归计算左半边最大子序和
+            max_left = self.maxSubArray(nums[0:len(nums) // 2])
+            #递归计算右半边最大子序和
+            max_right = self.maxSubArray(nums[len(nums) // 2:len(nums)])
+        
+        #计算中间的最大子序和，从右到左计算左边的最大子序和，从左到右计算右边的最大子序和，再相加
+        max_l = nums[len(nums) // 2 - 1]
+        tmp = 0
+        for i in range(len(nums) // 2 - 1, -1, -1):
+            tmp += nums[i]
+            max_l = max(tmp, max_l)
+        max_r = nums[len(nums) // 2]
+        tmp = 0
+        for i in range(len(nums) // 2, len(nums)):
+            tmp += nums[i]
+            max_r = max(tmp, max_r)
+        #返回三个中的最大值
+        return max(max_right,max_left,max_l+max_r)
+
+        
+        
+
+        
+# @lc code=end
+
